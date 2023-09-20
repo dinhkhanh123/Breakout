1273f684a2aec257616042530fd2a60458f59869
Khánh Trần Đình <trandinhkhanh0318@gmail.com>
2023-09-20 21:44:07 +0700

    Initial commit

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Vector2Int size;
+    public Vector2 offset;
+    public GameObject brickPrefab;
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < size.x; i++)
+        {
+            for(int j = 0; j < size.y; j++)
+            {
+                GameObject newBrick = Instantiate(brickPrefab,transform);
+                newBrick.transform.position = transform.position + new Vector3((float) ((size.x-1) * .5f - i)* offset.x ,j * offset.y ,0);
+
+            }
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
